058d3f7778a8aa46aed106d8b6da4d6dc3dd9889
leonardo060190 <leonardo-morruga@hotmail.com>
2023-11-30 16:13:36 -0300

    fazendo o post

--- src/compoments/form/Select.js
+++ src/compoments/form/Select.js
@@ -1,10 +1,15 @@
 import styles from './Select.module.css';
 
-function Select({ text, name, options, handleOnChange, value }) {
+function Select({ text, name, options = [], handleOnChange, value }) {
+    if (!Array.isArray(options)) {
+        console.error('Options should be an array.');
+        return null;
+    }
+    
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
                 <option>Selecione uma opção</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>{option.name}</option>
--- src/compoments/pages/Projects.js
+++ src/compoments/pages/Projects.js
@@ -1,6 +1,33 @@
-function Projects(){
-    return <h1>Projects</h1>
-        
+import { useLocation } from 'react-router-dom'
+
+import Message from "../layout/Message"
+import styles from "./Projects.module.css"
+import Container from "../layout/Container"
+import LinkButton from '../layout/LinkButton'
+
+
+function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.Message
     }
-    
-    export default Projects
+
+    return (
+        <div className={styles.prject_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+        </div>
+    )
+
+}
+
+export default Projects
